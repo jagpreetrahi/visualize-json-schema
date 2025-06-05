7d0d2cbabbf9139f9de14d60cabfa74d347f957c
jagpreetrahi <jagpreet1615@gmail.com>
2025-06-05 11:13:36 +0530

    Bug- Fix the base path for loading the CSS and JS

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : 'visualize-json-schema/',
+  base : '/visualize-json-schema/',
   plugins: [react()],
 })
