7d130b2ad0cda4b5624377ecd16b64724d462c83
jagpreetrahi <jagpreet1615@gmail.com>
2025-06-22 16:40:08 +0530

    Update the main file by removing the unused code for only render the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,19 @@
-
 import BottomBar from "./components/BottomBar"
 import NavigationBar from "./components/NavigationBar"
-import MonacoEditor from "./components/Monaco-Editor"
-import { useEffect, useRef, useState } from "react";
-import { BsArrowsFullscreen } from "react-icons/bs";
-import * as monaco from 'monaco-editor';
- 
-
-
+import MonacoEditor from "./components/MonacoEditor"
+import { MonacoEditorProvider } from "./contexts/EditorContext"
+import './App.css'
 
 function App() {
-
-  const editorRef =  useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-  
-  const [isEditorFullScreen , setEditorFullScreen] = useState(false);
-
-  const editorHeight : string = '80vh'
-  const editorWidth : string = '100%'
-
-  function ToggleScreen() : void{
-      setEditorFullScreen((prev) => !prev);
-  }
-
-  const togglebutton = (
-     <button onClick={ToggleScreen} className="cursor-pointer"><BsArrowsFullscreen size={25}/></button>
-  )
-
-  useEffect(() => {
-
-    function handleScreen(){
-        if(editorRef.current){
-            editorRef.current.layout
-        }
-    }
-    window.addEventListener('resize' , handleScreen)
-
-    return  () => window.removeEventListener('resize' , handleScreen)
-
-      
-  }, [])
-
-
   return (
-      isEditorFullScreen ? (
-            <MonacoEditor height={editorHeight} width={editorWidth} togglebutton={togglebutton}/>
-      ) : (
-        <>
-        <NavigationBar togglebutton={togglebutton}/>
-        <MonacoEditor height={editorHeight} width={editorWidth}/>
-          <BottomBar />
-        </>
-          
-      )
-    )
+    <>
+      <MonacoEditorProvider>
+        <NavigationBar/>
+        <MonacoEditor/>
+        </MonacoEditorProvider>
+      <BottomBar />
+    </>
+  )
       
 }
-
-
 export default App
