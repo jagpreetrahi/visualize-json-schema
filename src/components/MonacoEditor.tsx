d2e477c23a44a248dfb13dea72b0b948e0221bb7
jagpreetrahi <jagpreet1615@gmail.com>
2025-06-22 16:21:09 +0530

    Remove the unnecessary code and use the context from context folder into the file, and also import the schemaVisualization component for render on the page

---
+++ src/components/MonacoEditor.tsx
@@ -0,0 +1,49 @@
+import Editor from '@monaco-editor/react';
+import schema from '../data/dummy-schema.json'
+import {  useContext} from 'react';
+import { MonacoEditorContext } from '../contexts/EditorContext';
+import * as monaco from 'monaco-editor';
+import  {SchemaVisualization} from './SchemaVisualization';
+
+const  MonacoEditor = () => {
+    const {editorRef,  editorHeight, editorWidth , isEditorFullScreen} = useContext(MonacoEditorContext);
+
+    {/*Assign the editor instance when the editor's mounted */}
+    function MonacoEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
+        editorRef.current = editor;
+    }
+
+    return (
+        <>
+           <div className={`${isEditorFullScreen ? "fixed inset-0 z-50 bg-white p-4" : "relative z-10"}`}>
+                {/* <div className='flex justify-end items-center mt-1 mr-9 md:mr-5 px-2 py-1 md:px-4 md:py-2'>
+                    {toggleButton}
+                </div> */}
+
+                <div className="flex flex-col">
+
+                    <div className="flex flex-1 ">
+                        {/* JSON Schema Editor */}
+                        <div className="flex-1  overflow-hidden">
+                            <Editor height={editorHeight} width={editorWidth} defaultLanguage="json" defaultValue={JSON.stringify(schema , null , 2)}  theme='vs-dark' onMount={MonacoEditorDidMount} options={{scrollbar : {horizontal : 'hidden'} , minimap : {enabled : false}}}/>
+                            {/* Validation Result Panel */}
+                            <div className="border rounded  h-1/5  p-4 bg-neutral-800 text-white text-sm">
+                                <div className="flex justify-between items-center">
+                                    <h3>Validation Result</h3>
+                                </div>
+                               <pre className="mt-2 whitespace-pre-wrap">{}</pre>
+                            </div>
+                        </div>
+                        <SchemaVisualization/>
+                    </div>
+
+                    
+                  
+                </div>
+           </div>
+        </>
+    )
+     
+}
+export default MonacoEditor
+
