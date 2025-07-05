61bcfe7e64fa1e17dde75fe0fe4643c14c0ede83
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-05 15:19:42 +0530

    Render the ToolSummary component on the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import NavigationBar from "./components/NavigationBar"
 import MonacoEditor from "./components/MonacoEditor"
 import { MonacoEditorProvider } from "./contexts/EditorContext"
 import './App.css'
+import ToolSummary from "./components/ToolSummary"
 
 function App() {
   return (
     <>
       <MonacoEditorProvider>
         <NavigationBar/>
+        <ToolSummary/>
         <MonacoEditor/>
-        </MonacoEditorProvider>
+      </MonacoEditorProvider>
       <BottomBar />
     </>
   )
