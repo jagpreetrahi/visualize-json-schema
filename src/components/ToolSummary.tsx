2a6192b7f3d14f0ff33f17f23959977245b580c6
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-05 15:15:05 +0530

    Enhacement-Added the tool summary on the page

---
+++ src/components/ToolSummary.tsx
@@ -0,0 +1,22 @@
+import ToolData from './../data/tool-title-data.json'
+
+const ToolSummary = () => {
+
+    return (
+        <div className='w-full mx-auto mt-5'>
+           <div className='flex flex-col items-center'>
+               <div className='text-2xl text-blue-900' style={{letterSpacing : '2px', fontFamily : 'Roboto, sans-serif' }}>
+                   {ToolData.title}
+               </div>
+               <div className='max-w-3xl text-md'>
+                  {ToolData.content}
+               </div>
+
+
+           </div>
+
+        </div>
+    )
+}
+
+export default ToolSummary
