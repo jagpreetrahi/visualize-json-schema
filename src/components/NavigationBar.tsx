3752135c69ef388be901ae49132e7a3fb3592287
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-07 11:21:28 +0530

    Working on add the custom variable colors to the classname, still not finish yet will do later

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,23 +1,24 @@
-import { BsBrightnessHigh } from "react-icons/bs";
+import { BsBrightnessHigh, BsMoon } from "react-icons/bs";
 import { FaGithub, FaSearch } from "react-icons/fa";
 import { MonacoEditorContext } from "../contexts/EditorContext";
+import {useTheme} from "../contexts/ThemeContext"
 import { useContext} from "react";
 import { Tooltip } from 'react-tooltip'
 
 const  NavigationBar = () => {
   const {toggleButton} = useContext(MonacoEditorContext);
-
+  const {theme, toggleTheme} = useTheme();
   return (
     <>
       <nav className="p-3 flex justify-between items-center">
         <div className="flex space-x-2">
           <img src="json-icon.png" alt="JSON Schema logo" className="w-15 h-15 md:w-16 md:h-16" />
-          <span className="text-md sm:text-xl md:text-2xl font-semibold text-gray-800">JSON<br />Schema</span>
+          <span className="text-md sm:text-xl md:text-2xl font-semibold text-gray-600">JSON<br />Schema</span>
         </div>
         <ul className="flex gap-5 mr-10">
           <li>
-            <button aria-label="Toggle Theme" className="text-xl">
-              <BsBrightnessHigh />
+            <button aria-label="Toggle Theme" className="text-xl cursor-pointer" onClick={toggleTheme}>
+              {theme === 'light' ? <BsMoon/> : <BsBrightnessHigh />}
             </button>
           </li>
           <li>
