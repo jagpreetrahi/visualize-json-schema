07d183e50eb0eea91a78f6c87dbccc6e95b08e97
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-07 11:16:57 +0530

    Wrap all the components into the theme provider to use the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import BottomBar from "./components/BottomBar"
 import NavigationBar from "./components/NavigationBar"
 import MonacoEditor from "./components/MonacoEditor"
 import { MonacoEditorProvider } from "./contexts/EditorContext"
+import { ThemeProvider } from "./contexts/ThemeContext"
 import './App.css'
+import './styles/theme.css' 
 import ToolSummary from "./components/ToolSummary"
 
 function App() {
   return (
     <>
-      <MonacoEditorProvider>
-        <NavigationBar/>
-        <ToolSummary/>
-        <MonacoEditor/>
-      </MonacoEditorProvider>
-      <BottomBar />
+       <ThemeProvider>
+          <MonacoEditorProvider>
+            <NavigationBar/>
+            <ToolSummary/>
+            <MonacoEditor/>
+          </MonacoEditorProvider>
+          <BottomBar />
+       </ThemeProvider>
+    
     </>
   )
       
