08d85ba940bfd4e558f601fe016067da86754619
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-18 15:37:13 +0530

    Everythings up to date

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import MonacoEditor from "./components/MonacoEditor"
 import { MonacoEditorProvider } from "./contexts/EditorContext"
 import { ThemeProvider } from "./contexts/ThemeContext"
 import './App.css'
-import './styles/theme.css' 
 import ToolSummary from "./components/ToolSummary"
 
 function App() {
   return (
     <>
-       <ThemeProvider>
-          <MonacoEditorProvider>
-            <NavigationBar/>
-            <ToolSummary/>
-            <MonacoEditor/>
-          </MonacoEditorProvider>
-          <BottomBar />
-       </ThemeProvider>
-    
+      <ThemeProvider>
+        <MonacoEditorProvider>
+          <NavigationBar/>
+          <ToolSummary/>
+          <MonacoEditor/>
+        </MonacoEditorProvider>
+        <BottomBar />
+      </ThemeProvider>
     </>
   )
       
