e2010862e8d2f0475d4be4341848650da15b1402
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-18 15:24:33 +0530

    Enhance the editor by adding the content related to view and resize on top of it

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -3,11 +3,14 @@ import schema from '../data/dummy-schema.json'
 import {useContext, useState} from 'react';
 import { MonacoEditorContext } from '../contexts/EditorContext';
 import * as monaco from 'monaco-editor';
+import { CgChevronDown } from "react-icons/cg";
 import SchemaVisualization from './SchemaVisualization';
 import {Panel, PanelGroup, PanelResizeHandle} from 'react-resizable-panels'
 
+
+
 const MonacoEditor = () => {
-    const {editorRef,  editorHeight, editorWidth , isFullScreen, containerRef} = useContext(MonacoEditorContext);
+    const {editorRef,  editorHeight, editorWidth , isFullScreen, containerRef, toggleButton} = useContext(MonacoEditorContext);
     // Extract the schema to a state so that react tracks the schema updates
     const [schemaValue, setSchemaValue] = useState(JSON.stringify(schema, null, 2));
     //define the panel size for editor and visualization
@@ -20,8 +23,42 @@ const MonacoEditor = () => {
     function MonacoEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
         editorRef.current = editor;
     }
+ 
+    // state for toggle the view visibility
+    const view = ['Graph' , 'Tree']
+    const [isView, setInView] = useState(false); 
+    const [isSelected , setIsSelected] = useState('Graph');
     return (
-       <div ref={containerRef} className={`${isFullScreen ? "fixed inset-0 z-50 p-4" : "relative z-10"}`}  >
+       <div ref={containerRef} className={`${isFullScreen ? "fixed inset-0 z-50 " : "relative z-10"}`}  >
+            <div className='w-full px-2 py-1 bg-neutral-800'>
+                <div className='flex flex-row justify-between'>
+                    <div className='flex flex-row space-x-5'>
+                        <span className='text-md text-white'>JSON Schema</span>
+                        <div className='relative'>
+                            <button className='text-white rounded-md px-1 flex flex-row items-center gap-x-1 hover:bg-white/10' onClick={() => setInView(!isView)}> View 
+                                <CgChevronDown size={12} />
+                            </button>   
+                            {isView && (
+                                <div className='absolute left-0 top-full mt-2 bg-neutral-700 rounded-sm px-2 py-1 z-50'>
+                                    <ul className='flex flex-row gap-x-2 text-white'>
+                                        {view.map((item) => (
+                                          <li key={item}>
+                                             <button onClick={() => setIsSelected(item)} className={`${isSelected === item ? 'bg-neutral-500 text-white' : 'bg-transparent text-gray-200 hover:bg-neutral-500'} px-2 py-1 rounded cursor-pointer`}>{item}</button>
+                                          </li>
+                                        ) )}
+                                    </ul>
+                               </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className='mr-5 text-white'>
+                        {toggleButton}
+                    </div>
+                </div>
+            </div>
+          
+           
+           
           <PanelGroup direction="horizontal">
               <Panel maxSize={editorPanelMaxSize} minSize={editorPanelMinSize} defaultSize={editorPanelMaxSize}>
                   <div className='flex flex-col h-[100vh] gap-y-1'>
@@ -49,5 +86,6 @@ const MonacoEditor = () => {
           </PanelGroup>
        </div>
 )}
+
 export default MonacoEditor
 
