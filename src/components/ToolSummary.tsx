ac08a98d8b244e1bac3ab6beb089d6eecbab10e3
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-20 13:47:05 +0530

    Added the style for tool content using css variable

--- src/components/ToolSummary.tsx
+++ src/components/ToolSummary.tsx
@@ -1,22 +1,16 @@
 import ToolData from './../data/tool-title-data.json'
-
 const ToolSummary = () => {
-
     return (
         <div className='w-full mx-auto mt-5'>
            <div className='flex flex-col items-center'>
-               <div className='text-2xl text-blue-900' style={{letterSpacing : '2px', fontFamily : 'Roboto, sans-serif' }}>
+               <div className='text-2xl text-[var(--tool-name-color)]' style={{letterSpacing : '2px', fontFamily : 'Roboto, sans-serif' }}>
                    {ToolData.title}
                </div>
-               <div className='max-w-3xl text-md'>
+               <div className='max-w-3xl text-md text-[var(--tool-content-color)]'>
                   {ToolData.content}
                </div>
-
-
-           </div>
-
-        </div>
+            </div>
+       </div>
     )
 }
-
 export default ToolSummary
