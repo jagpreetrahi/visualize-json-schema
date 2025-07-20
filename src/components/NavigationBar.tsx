a699104d21328a87ef97037cabba48676535bc37
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-20 13:49:00 +0530

    Enhance-Added the new tool logo and also use the theme context for toggling

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { BsBrightnessHigh, BsMoon } from "react-icons/bs";
+import { BsBrightnessHigh,} from "react-icons/bs";
 import { FaGithub, FaSearch } from "react-icons/fa";
 import { MonacoEditorContext } from "../contexts/EditorContext";
 import {useTheme} from "../contexts/ThemeContext"
@@ -7,29 +7,29 @@ import { Tooltip } from 'react-tooltip'
 
 const  NavigationBar = () => {
   const {toggleButton} = useContext(MonacoEditorContext);
-  const {theme, toggleTheme} = useTheme();
+  const {toggleTheme} = useTheme();
   return (
     <>
       <nav className="p-3 flex justify-between items-center">
-        <div className="flex space-x-2">
-          <img src="json-icon.png" alt="JSON Schema logo" className="w-15 h-15 md:w-16 md:h-16" />
-          <span className="text-md sm:text-xl md:text-2xl font-semibold text-gray-600">JSON<br />Schema</span>
+        <div className="flex items-center">
+          <img src="tool_image.png" alt="JSON Schema logo" className="w-12 h-12 md:w-16 md:h-16"/>
+          <span className="sm:text-xl md:text-xl leading-none font-semibold text-[var(--navigation-text-color)]">JSON<br />Schema</span>
         </div>
         <ul className="flex gap-5 mr-10">
           <li>
             <button aria-label="Toggle Theme" className="text-xl cursor-pointer" onClick={toggleTheme}>
-              {theme === 'light' ? <BsMoon/> : <BsBrightnessHigh />}
+             <BsBrightnessHigh style={{ color: 'var(--navigation-text-color)' }} />
             </button>
           </li>
           <li>
             <a href="https://github.com/jagpreetrahi/visualize-json-schema" target="_blank" rel="noopener noreferrer" className="text-xl" data-tooltip-id="github" data-tooltip-content="Star on Github">
-              <FaGithub/>
+             <FaGithub style={{ color: 'var(--navigation-text-color)' }} />
               <Tooltip id="github"/>
             </a>
           </li>
           <li>
             <a href="https://www.learnjsonschema.com/2020-12/" target="_blank" rel="noopener noreferrer" className="text-xl" data-tooltip-id="learn-keywords" data-tooltip-content="Explore Keywords">
-              <FaSearch />
+              <FaSearch style={{ color: 'var(--navigation-text-color)' }}/>
               <Tooltip id="learn-keywords" />
             </a>
           </li>
