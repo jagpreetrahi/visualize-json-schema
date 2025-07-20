dd0adedfad4dd9f77072ffd7e996e9b09aed9ad7
jagpreetrahi <jagpreet1615@gmail.com>
2025-07-20 13:44:26 +0530

    Import the theme style into it

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,21 @@ import NavigationBar from "./components/NavigationBar"
 import MonacoEditor from "./components/MonacoEditor"
 import { MonacoEditorProvider } from "./contexts/EditorContext"
 import { ThemeProvider } from "./contexts/ThemeContext"
+import './style/theme.css'
 import './App.css'
-import './styles/theme.css' 
 import ToolSummary from "./components/ToolSummary"
 
 function App() {
   return (
     <>
-       <ThemeProvider>
+      <ThemeProvider>
           <MonacoEditorProvider>
             <NavigationBar/>
             <ToolSummary/>
             <MonacoEditor/>
           </MonacoEditorProvider>
           <BottomBar />
-       </ThemeProvider>
-    
+      </ThemeProvider>
     </>
   )
       
