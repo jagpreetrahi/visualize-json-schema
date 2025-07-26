98fd53e7a96fdddf2464b0686b0ba70ef2054bb8
Agnivesh Chaubey <agniveshvapi@gmail.com>
2025-07-26 16:16:20 +0530

    fix ToolSummary component
    Signed-off-by: Agnivesh Chaubey <agniveshvapi@gmail.com>

--- src/components/ToolSummary.tsx
+++ src/components/ToolSummary.tsx
@@ -1,16 +1,19 @@
-import ToolData from './../data/tool-title-data.json'
+import toolSummary from "../data/toolSummary.json";
+
 const ToolSummary = () => {
-    return (
-        <div className='w-full mx-auto mt-5'>
-           <div className='flex flex-col items-center'>
-               <div className='text-2xl text-[var(--tool-name-color)]' style={{letterSpacing : '2px', fontFamily : 'Roboto, sans-serif' }}>
-                   {ToolData.title}
-               </div>
-               <div className='max-w-3xl text-md text-[var(--tool-content-color)]'>
-                  {ToolData.content}
-               </div>
-            </div>
-       </div>
-    )
-}
-export default ToolSummary
+  return (
+    <div className="flex flex-col items-center">
+      <div
+        className="text-2xl text-[var(--tool-name-color)]"
+        style={{ letterSpacing: "2px", fontFamily: "Roboto, sans-serif" }}
+      >
+        {toolSummary.title}
+      </div>
+      <div className="max-w-3xl text-md text-[var(--tool-content-color)]">
+        {toolSummary.summary}
+      </div>
+    </div>
+  );
+};
+
+export default ToolSummary;
