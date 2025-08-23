feb137b08933f88545d779253e62956d1b47028e
Agnivesh Chaubey <agniveshvapi@gmail.com>
2025-08-23 22:55:14 +0530

    add handlers to generate nodes and edges from json schema
    Signed-off-by: Agnivesh Chaubey <agniveshvapi@gmail.com>

--- src/components/temp-graph.tsx
+++ src/components/temp-graph.tsx
@@ -1,105 +1,125 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import {
   ReactFlow,
   Background,
   Controls,
   useNodesState,
   useEdgesState,
+  Position,
+  type Node,
+  type Edge,
 } from "@xyflow/react";
 
 import dagre from "@dagrejs/dagre";
 import "@xyflow/react/dist/style.css";
-
-const initialNodes = [
-  {
-    id: "n1",
-    data: { label: "Root Node" },
-    position: { x: 250, y: 0 },
-  },
-  {
-    id: "n2",
-    data: { label: "Child A" },
-    position: { x: 150, y: 150 },
-  },
-  {
-    id: "n3",
-    data: { label: "Grandchild A1" },
-    position: { x: 100, y: 300 },
-  },
-  {
-    id: "n4",
-    data: { label: "Child B" },
-    position: { x: 350, y: 150 },
-  },
-];
-
-const initialEdges = [
-  {
-    id: "e1-2",
-    source: "n1",
-    target: "n2",
-  },
-  {
-    id: "e1-4",
-    source: "n1",
-    target: "n4",
-  },
-  {
-    id: "e2-3",
-    source: "n2",
-    target: "n3",
-  },
-];
 
 const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
 
 const nodeWidth = 172;
 const nodeHeight = 36;
 
-const getLayoutedElements = (nodes, edges, direction = "LR") => {
-  const isHorizontal = direction === "LR";
-  dagreGraph.setGraph({ rankdir: direction });
-
-  nodes.forEach((node) => {
-    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-  });
-
-  edges.forEach((edge) => {
-    dagreGraph.setEdge(edge.source, edge.target);
-  });
-
-  dagre.layout(dagreGraph);
-
-  const newNodes = nodes.map((node) => {
-    const nodeWithPosition = dagreGraph.node(node.id);
-    const newNode = {
-      ...node,
-      targetPosition: isHorizontal ? "left" : "top",
-      sourcePosition: isHorizontal ? "right" : "bottom",
-      // We are shifting the dagre node position (anchor=center center) to the top left
-      // so it matches the React Flow node anchor point (top left).
-      position: {
-        x: nodeWithPosition.x - nodeWidth / 2,
-        y: nodeWithPosition.y - nodeHeight / 2,
-      },
-    };
-
-    return newNode;
-  });
-
-  return { nodes: newNodes, edges };
+type GraphNode = {
+  id: string;
+  data: { label: string };
 };
 
-const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-  initialNodes,
-  initialEdges
-);
+type GraphEdge = {
+  id: string;
+  source: string;
+  target: string;
+};
 
+const TempGraph = ({ schema }: { schema: string }) => {
+  const [nodes, setNodes, onNodeChange] = useNodesState<Node>([]);
+  const [edges, setEdges, onEdgeChange] = useEdgesState<Edge>([]);
 
-const TempGraph = ({ schema }) => {
-  const [nodes, setNodes, onNodeChange] = useNodesState(layoutedNodes);
-  const [edges, setEdges, onEdgeChange] = useEdgesState(layoutedEdges);
+  const generateNodesAndEdges = useCallback(
+    (
+      schema: JSON,
+      parentNodeId: string,
+      nodes: GraphNode[] = [],
+      edges: GraphEdge[] = []
+    ) => {
+      if (parentNodeId === "root") {
+        nodes.push({ id: `${parentNodeId}`, data: { label: "root" } });
+      }
+      for (const [key, value] of Object.entries(schema)) {
+        const isExpandable = value !== null && typeof value === "object";
+        const currentNodeId = `${parentNodeId}-${key}`;
 
+        const newNode = {
+          id: currentNodeId,
+          data: { label: `${key} -- ${value}` },
+        };
+
+        const newEdge = {
+          id: `${parentNodeId}-${currentNodeId}`,
+          source: parentNodeId,
+          target: currentNodeId,
+        };
+
+        if (isExpandable) {
+          generateNodesAndEdges(value, currentNodeId, nodes, edges);
+        }
+        nodes.push(newNode);
+        edges.push(newEdge);
+      }
+      return { nodes, edges };
+    },
+    []
+  );
+
+  const getLayoutedElements = useCallback(
+    (nodes: GraphNode[], edges: GraphEdge[], direction = "LR") => {
+      const isHorizontal = direction === "LR";
+      dagreGraph.setGraph({ rankdir: direction });
+
+      nodes.forEach((node) => {
+        dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+      });
+
+      edges.forEach((edge) => {
+        dagreGraph.setEdge(edge.source, edge.target);
+      });
+
+      dagre.layout(dagreGraph);
+
+      const newNodes = nodes.map((node) => {
+        const nodeWithPosition = dagreGraph.node(node.id);
+        const newNode: Node = {
+          ...node,
+          targetPosition: isHorizontal ? Position.Left : Position.Top,
+          sourcePosition: isHorizontal ? Position.Right : Position.Bottom,
+          // We are shifting the dagre node position (anchor=center center) to the top left
+          // so it matches the React Flow node anchor point (top left).
+          position: {
+            x: nodeWithPosition.x - nodeWidth / 2,
+            y: nodeWithPosition.y - nodeHeight / 2,
+          },
+        };
+
+        return newNode;
+      });
+
+      return { nodes: newNodes, edges };
+    },
+    []
+  );
+
+  useEffect(() => {
+    const { nodes: rawNodes, edges: rawEdges } = generateNodesAndEdges(
+      JSON.parse(schema),
+      "root"
+    );
+
+    const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+      rawNodes,
+      rawEdges
+    );
+
+    setNodes(layoutedNodes);
+    setEdges(layoutedEdges);
+  }, [schema, generateNodesAndEdges, getLayoutedElements, setNodes, setEdges]);
 
   return (
     <div className="bg-gray-200" style={{ width: "100%", height: "100%" }}>
