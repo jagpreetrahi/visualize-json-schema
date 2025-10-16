3c1f15b058adac365b801e948ee31540d049519f
jagpreetrahi <jagpreet1615@gmail.com>
2025-10-16 13:04:41 +0530

    Update the theme value in the context

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -3,9 +3,8 @@ import { createContext, type Ref } from "react";
 type AppContextType = {
   containerRef: Ref<HTMLDivElement>;
   isFullScreen: boolean;
+  theme: "light" | "dark",
   toggleFullScreen: () => void;
-
-  theme: string;
   toggleTheme: () => void;
 };
 
