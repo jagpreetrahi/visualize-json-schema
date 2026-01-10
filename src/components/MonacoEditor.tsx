858833ecb0db0ccb7c988acd1149dc8e03138a4d
Agnivesh Chaubey <agniveshvapi@gmail.com>
2026-01-10 14:15:33 +0530

    fix: allow boolean schemas and schemas without $schema keyword, but with a warning
    Signed-off-by: Agnivesh Chaubey <agniveshvapi@gmail.com>

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,83 +1,97 @@
-import { useCallback, useContext, useState, useRef, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import {
   registerSchema,
   unregisterSchema,
+  type SchemaObject,
 } from "@hyperjump/json-schema/draft-2020-12";
 import {
   getSchema,
   compile,
   type CompiledSchema,
 } from "@hyperjump/json-schema/experimental";
-import * as monaco from "monaco-editor";
 import Editor from "@monaco-editor/react";
-import schema from "../data/defaultJSONSchema.json";
+import defaultSchema from "../data/defaultJSONSchema.json";
 import { AppContext } from "../contexts/AppContext";
 import SchemaVisualization from "./SchemaVisualization";
 import FullscreenToggleButton from "./FullscreenToggleButton";
 
-type ValidationState =
-  | { status: "idle" }
-  | { status: "valid" }
-  | { status: "error"; message: string };
+type ValidationStatus = {
+  status: "success" | "warning" | "error";
+  message: string;
+};
 
 const MonacoEditor = () => {
   const { theme, isFullScreen, containerRef } = useContext(AppContext);
-  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-  const [validationState, setValidationState] = useState<ValidationState>({
-    status: "idle",
-  });
+
   const [compiledSchema, setCompiledSchema] = useState<CompiledSchema | null>(
     null
   );
-  const [schemaValue, setSchemaValue] = useState<string | undefined>(
-    window.sessionStorage.getItem("JSON Schema") ??
-      JSON.stringify(schema, null, 2)
+
+  const DEFAULT_SCHEMA_DIALECT = "https://json-schema.org/draft/2020-12/schema";
+  const DEFAULT_SCHEMA_ID = "https://example.com/schema";
+  const SESSION_STORAGE_KEY = "JSON Schema";
+  const VALIDATION_MESSAGE = {
+    success: "✓ Valid JSON Schema",
+    warning: `⚠ Schema dialect not provided. Using default dialect, that is ${DEFAULT_SCHEMA_DIALECT}`,
+  };
+  const statusClassMap: Record<ValidationStatus["status"], string> = {
+    error: "text-red-400",
+    warning: "text-yellow-400",
+    success: "text-green-400 font-semibold",
+  };
+
+  const [schemaText, setSchemaText] = useState<string>(
+    window.sessionStorage.getItem(SESSION_STORAGE_KEY)?.trim() ??
+      JSON.stringify(defaultSchema, null, 2)
   );
 
-  const compileSchema = useCallback(
-    async (value: string | undefined) => {
-      if (!value) return;
-
-      let schemaId: string;
-      try {
-        setValidationState({ status: "idle" });
-        const parsedSchema = JSON.parse(value);
-        schemaId = parsedSchema.$id;
-
-        unregisterSchema(schemaId);
-        registerSchema(parsedSchema, schemaId);
-
-        const schemaDocument = await getSchema(schemaId);
-        const compiledSchema = await compile(schemaDocument);
-
-        setCompiledSchema(compiledSchema);
-        setValidationState({ status: "valid" });
-        if (value !== schemaValue) {
-          setSchemaValue(value);
-        }
-        window.sessionStorage.setItem("JSON Schema", value);
-      } catch (err) {
-        setCompiledSchema(null);
-        setValidationState({
-          status: "error",
-          message: err instanceof Error ? err.message : String(err),
-        });
-      }
-    },
-    [schemaValue]
-  );
+  const [schemaValidation, setSchemaValidation] = useState<ValidationStatus>({
+    status: "success",
+    message: VALIDATION_MESSAGE["success"],
+  });
 
   useEffect(() => {
-    compileSchema(schemaValue);
-  }, [compileSchema, schemaValue]);
+    window.sessionStorage.setItem(SESSION_STORAGE_KEY, schemaText);
+  }, [schemaText]);
 
-  const handleEditorMount = useCallback(
-    (editor: monaco.editor.IStandaloneCodeEditor) => {
-      editorRef.current = editor;
-    },
-    []
-  );
+  useEffect(() => {
+    if (!schemaText.trim()) return;
+
+    (async () => {
+      try {
+        const parsed = JSON.parse(schemaText);
+
+        const schemaDialect = parsed.$schema ?? DEFAULT_SCHEMA_DIALECT;
+        const schemaId = parsed.$id ?? DEFAULT_SCHEMA_ID;
+
+        unregisterSchema(schemaId);
+        registerSchema(parsed, schemaId, schemaDialect);
+
+        const schemaDoc = await getSchema(schemaId);
+
+        setCompiledSchema(await compile(schemaDoc));
+        setSchemaValidation(
+          parsed.$schema
+            ? {
+                status: "success",
+                message: VALIDATION_MESSAGE["success"],
+              }
+            : {
+                status: "warning",
+                message: VALIDATION_MESSAGE["warning"],
+              }
+        );
+      } catch (err) {
+        const message = err instanceof Error ? err.message : String(err);
+
+        setSchemaValidation({
+          status: "error",
+          message,
+        });
+      }
+    })();
+  }, [schemaText]);
 
   return (
     <div ref={containerRef} className="h-[92vh] flex flex-col">
@@ -94,23 +108,16 @@ const MonacoEditor = () => {
             height="90%"
             width="100%"
             defaultLanguage="json"
-            value={schemaValue}
+            value={schemaText}
             theme={theme === "light" ? "vs-light" : "vs-dark"}
-            onMount={handleEditorMount}
             options={{ minimap: { enabled: false } }}
-            onChange={compileSchema}
+            onChange={(value) => setSchemaText(value ?? "")}
           />
-          {validationState.status !== "idle" && (
-            <div className="flex-1 p-2 bg-[var(--validation-bg-color)] text-sm overflow-y-auto">
-              {validationState.status === "error" ? (
-                <div className="text-red-400">{validationState.message}</div>
-              ) : (
-                <div className="text-green-400 font-semibold">
-                  ✓ Valid JSON Schema
-                </div>
-              )}
+          <div className="flex-1 p-2 bg-[var(--validation-bg-color)] text-sm overflow-y-auto">
+            <div className={statusClassMap[schemaValidation.status]}>
+              {schemaValidation.message}
             </div>
-          )}
+          </div>
         </Panel>
         <PanelResizeHandle className="w-[1px] bg-gray-400" />
         <Panel
